3922f254319c7d6054841e053b17c30174db15e5
1kJae <wnstjq62@naver.com>
2025-05-15 16:32:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Homepage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundpage/NotFoundPage';
+
+// 홈페이지 /
+// 영화 전체를 보여주는 페이지 (서치) /movies
+// 영화 디테일 페이지 /movies/:id
+// 추천 영화 /movies/:id/recommandation
+// 리뷰 /movies/:id/reviews
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" element={<AppLayout/>}>
+            <Route index element={<Homepage/>} />
+            <Route path='movies'>
+              <Route index element={<MoviePage/>} />
+              <Route path=':id' element={<MovieDetailPage/>} />
+            </Route>
+          </Route>
+
+          <Route path='*' element={<NotFoundPage/>} />
+        </Routes>
     </div>
   );
 }
