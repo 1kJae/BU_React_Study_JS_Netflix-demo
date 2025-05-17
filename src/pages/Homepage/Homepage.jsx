c9afc6f10a84de69c181721ccca90c5b7309db56
1kJae <wnstjq62@naver.com>
2025-05-17 18:01:51 +0900

    0517 commit

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Banner from './components/Banner'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import PopularMovieSlide from './components/PopularMovieSlide/PopularMovieSlide';
 
 // 1. 배너 => popular 영화를 들고와서 첫번째 아이템을 보여주자.
 // 2. popular movie
@@ -8,8 +11,11 @@ import Banner from './components/Banner'
 
 const Homepage = () => {
   return (
-    <div><Banner/></div>
-  )
-}
+    <div>
+      <Banner />
+      <PopularMovieSlide />
+    </div>
+  );
+};
 
 export default Homepage
