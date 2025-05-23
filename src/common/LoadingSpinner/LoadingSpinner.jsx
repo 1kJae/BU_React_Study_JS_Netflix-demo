9ce25c1cb7b364836c3faba0c251e3e518327369
1kJae <wnstjq62@naver.com>
2025-05-23 15:36:57 +0900

    0523_3 commit

--- src/common/LoadingSpinner/LoadingSpinner.jsx
+++ src/common/LoadingSpinner/LoadingSpinner.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const LoadingSpinner = () => {
   return (
-    <div>LoadingSpinner</div>
-  )
-}
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: '60vh' }}
+    >
+      <Spinner animation="border" variant="danger" style={{ width: '5rem', height: '5rem' }} />
+    </div>
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
